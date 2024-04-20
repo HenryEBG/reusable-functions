8e0c7652a388c347319183b7335ab6345f63ea80
HenryEBG <berganza386@gmail.com>
2024-04-20 00:00:38 -0400

    adding solution to the Part 2 of the assestment

--- script.js
+++ script.js
@@ -127,16 +127,42 @@ let methodArray =
  { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
  { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
  { id: "7", name: "Bilbo", occupation: "None", age: "111" }]
- //console.log(methodArray)
-
  
- methodArray.sort(compareAges)
 
- function compareAges(a,b){
-    return a.age-b.age
- }
 
- console.log(methodArray)
+ //order the array by age
+methodArray.sort( (a,b) => a.age-b.age)
+
+console.log(`***** Order Array ****** `)
+console.log(methodArray)
+
+
+//filter the array by ages
+const filterArray=methodArray.filter(a => a.age<=50)
+
+console.log(`***** Filter Array removes greater than 50  ****** `)
+console.log(filterArray)
+
+
+//With map change the occupation with job and sum +1 to the age
+const mapArray = methodArray.map(person=>({
+  id:person.id,
+  name:person.name,
+  job:person.occupation,
+  age:parseInt(person.age)+1
+}))
+
+console.log(mapArray)
+
+
+//With reduce sum ages 
+
+const sumAges = mapArray.reduce((sumAge,person) => sumAge + parseInt(person.age),0)
+
+console.log(`The sum of the ages is : ${sumAges}`)
+
+console.log(`The average of the ages is : ${sumAges/mapArray.length}`)
+
 
 
  /**
